a51efdfc2d66e39fa6708922c5e25ca833abaf00
DieSILV <59703636+DieSILV@users.noreply.github.com>
2024-11-04 18:01:50 +0100

    done

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import "./NavBar.css"
+
+const NavBar = () => {
+    return (
+
+        <nav>
+
+            <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/discord-logo-white.png" alt="discordLogo" className='discordLogo' />
+
+            <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-icon.png" alt="menuIcon" />
+
+        </nav>
+    )
+}
+
+export default NavBar
